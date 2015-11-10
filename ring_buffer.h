da1708ecb60796425ae49c7ebb7f0bfafb74200d
kxp <kxp@users.noreply.github.com>
2015-11-10 23:29:58 +0000

    Added: notes

--- ring_buffer.h
+++ ring_buffer.h
@@ -2,7 +2,7 @@
 //#include <pthreads.h>
 #include <stdint.h>
 
-typedef struct ring_t
+typedef struct ring_t//some padding should be taking into consideration.
 {
     uint8_t *buffer_start;// number of thigs that u wanna store
     uint8_t *buffer_end;
@@ -17,25 +17,22 @@ typedef struct ring_t
     char round_complete; //0- false; 1- true
 }ring_t;
 
-extern "C"
-{
-    //buffern len is the number of pointer we want allocate, the ptr size is the maximum size of each ptr.
-    ring_t* rb_new(uint32_t buffer_len, uint32_t pointer_size);
+//buffern len is the number of pointer we want allocate, the ptr size is the maximum size of each ptr.
+ring_t* rb_new(uint32_t buffer_len, uint32_t pointer_size);
 
-    uint32_t rb_full(ring_t* rb);
+uint32_t rb_full(ring_t* rb);
 
-    uint32_t rb_empty(ring_t* rb);
+uint32_t rb_empty(ring_t* rb);
 
-    uint32_t rb_write(ring_t* rb, uint8_t *data, uint32_t size);//avpacket
+uint32_t rb_write(ring_t* rb, uint8_t *data, uint32_t size);//avpacket
 
-    uint32_t 	rb_read(ring_t* rb, uint32_t *size, uint8_t* memory);
+uint32_t 	rb_read(ring_t* rb, uint32_t *size, uint8_t* memory);
 
-    uint32_t 	rb_free_space(ring_t * rb);
+uint32_t 	rb_free_space(ring_t * rb);
 
-    uint32_t 	rb_filled_space(ring_t * rb);
+uint32_t 	rb_filled_space(ring_t * rb);
 
-    void	rb_release(ring_t * rb);
+void	rb_release(ring_t * rb);
 
-}
 
 
