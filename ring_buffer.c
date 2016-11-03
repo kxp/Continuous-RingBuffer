1a574e9636ea9f3b9a443691859ebf78c1821d61
kxp <kxp@users.noreply.github.com>
2016-11-03 21:48:08 +0000

    Crash fix

--- ring_buffer.c
+++ ring_buffer.c
@@ -5,22 +5,21 @@
 #define LOG_PRINT printf
 
 
-
- //buffern len is the number of pointers we want allocate, the ptr size is the maximum size of each ptr.
+//buffern len is the number of pointers we want allocate, the ptr size is the maximum size of each ptr.
 ring_t*
-rb_new(uint32_t buffer_len, uint32_t pointer_size)
+rb_new(uint32_t buffer_len)
 {   
     ring_t * rb;
     rb = (ring_t*)malloc(sizeof(ring_t));
     if (!rb)
-    	return NULL;
+        return NULL;
 
-    LOG_PRINT("\nInside of the ring buffer, ptrsize= %d , buffer len= %d", pointer_size, buffer_len);
+    LOG_PRINT("\nRing buffer is being created with the size %d.", buffer_len);
 
-    rb->size = buffer_len *pointer_size;
+    rb->size = buffer_len;
 
-    rb->buffer_start = (uint8_t*)malloc(rb->size * sizeof *rb->buffer_start);    
-    memset(rb->buffer_start, 0, rb->size);
+    rb->buffer_start = (uint8_t*)malloc(rb->size * sizeof *rb->buffer_start);
+        memset(rb->buffer_start, 0, rb->size);
     
     rb->buffer_end =  (rb->buffer_start + (rb->size ) );
     rb->write_calls = 0;
@@ -28,7 +27,7 @@ rb_new(uint32_t buffer_len, uint32_t pointer_size)
     rb->head = rb->buffer_start;
     rb->tail =  rb->buffer_end;
            
-    rb->round_complete = 1;
+    rb->round_complete = 0;
     
     return rb;
 }
@@ -64,7 +63,6 @@ rb_write(ring_t* rb, uint8_t *data, uint32_t size)
     {
         rb->write_calls++;//debug       
         int32_t avaiable_space = rb_free_space(rb);
-        printf("\nwrite: avaiable_space: %d\n", avaiable_space);
         if ( avaiable_space >= size)
         {
             memcpy(rb->head, data, size);
@@ -76,21 +74,22 @@ rb_write(ring_t* rb, uint8_t *data, uint32_t size)
             }
             return size;
         }
-        else if (avaiable_space >= 0)
+        else if (avaiable_space >= size)
         {
-            uint32_t remaining_size = (size - avaiable_space);
-            memcpy(rb->head, data, avaiable_space);
+            uint32_t remaining_size = (rb->buffer_end - rb->head);
+            memcpy(rb->head, data, remaining_size);
             rb->head = rb->buffer_start;
+            uint32_t missing_size = size - remaining_size;
+            if (missing_size <= 0)
+                return 0;
 
-            memcpy(rb->head, (data + avaiable_space), remaining_size);
-
-            rb->head = (rb->head + remaining_size );
+            memcpy(rb->head, (data + remaining_size), missing_size);
+            rb->head = (rb->head + missing_size);
             rb->round_complete = 1;
-            return size;            
+            return size;
         }
         else
-            LOG_PRINT("rb_write:Pointer to read is empty\n");
-        return 0;
+            LOG_PRINT("rb_write:No more available memory! You must abort!\n");
     }
     return 0;
 }
@@ -101,28 +100,14 @@ rb_read(ring_t* rb, uint32_t *size, uint8_t* memory)
     if (rb == NULL || rb->buffer_start == NULL)
         goto failed_end;   
     if (1 == rb_empty(rb))
-    	goto failed_end;
+        goto failed_end;
 
     rb->read_calls++;//if debug?
 
-    int32_t avaiable_space = (int32_t) (rb->buffer_end - rb->tail) ;
-	printf("\nread:  avaiable_space: %d",avaiable_space);
-    if (avaiable_space < *size && avaiable_space >= 0)
-    {
-        memcpy(memory, (rb->tail), avaiable_space );
-        uint32_t remaining_size = (*size - avaiable_space);       
-       
-        rb->tail = rb->buffer_start;
-       
-        uint8_t* temp = (memory + avaiable_space );
-        memcpy(temp, rb->buffer_start, remaining_size);
+    uint32_t available_space = rb_free_space(rb);
 
-        rb->tail = (rb->buffer_start + remaining_size );
-        rb->round_complete = 0;
-
-        return *size;
-    }
-    else if (avaiable_space > 0)
+    //Triple check!
+    if ((rb->tail + *size) <= rb->head)
     {
         memcpy(memory, (rb->tail), *size);
         rb->tail = rb->tail + *size;
@@ -131,13 +116,52 @@ rb_read(ring_t* rb, uint32_t *size, uint8_t* memory)
             rb->tail = rb->buffer_start;
             rb->round_complete = 0;
         }
-       return *size;
-    }    
+        return NULL;
+    }
+    else if (available_space < rb->size)
+    {
+        int32_t remaining_space = rb->buffer_end - rb->tail;
+       
+        if (remaining_space >= *size)//this shouldnt happen!this if should get caugth in the previous one
+        {
+            memcpy(memory, (rb->tail), *size);
+            rb->tail += *size;
+            if (rb->tail == rb->buffer_end)
+            {
+                rb->tail = rb->buffer_start;
+                rb->round_complete = 0;
+            }
+            return 0;
+        }
+        else
+        {
+            memcpy(memory, (rb->tail), remaining_space);
+            rb->tail += remaining_space;
+            //
+            if (rb->tail == rb->buffer_end)
+                rb->tail = rb->buffer_start;
+            else if (rb->tail > rb->buffer_end)
+                LOG_PRINT("rb_read:Ups, this is bad!\n");
+
+            //get the missing asked size
+            int32_t missing_size = *size - remaining_space;
+            //get the ptr
+            uint8_t* temp = (memory + remaining_space);
+            memcpy(temp, rb->tail, missing_size);//cpy the lefted ptr
+            rb->tail += missing_size;
+            rb->round_complete = 0;
+            return size;
+        }
+    }
+    else
+    {
+        LOG_PRINT("rb_read:Nothing to Read from the buffer!\n");
+    }
     
     failed_end:
-		*size = 0;
+        *size = 0;
 
-	return 0;
+    return 0;
 }
 
 
@@ -147,22 +171,21 @@ rb_free_space(ring_t * rb)
     if (rb == NULL)
         return 0;
     if (rb->round_complete == 1)
-        return(rb->tail - rb->head) ;
+    {
+        int32_t free_space = (int32_t)(rb->tail - rb->head);//later do a direct return
+        return free_space;
+    }
     else
-        return (rb->head - rb->tail);
+    {
+        int32_t free_space = (int32_t) (rb->head - rb->tail);//later do a direct return
+        if (free_space == 0)
+        {
+            free_space = rb->size;
+        }
+        return free_space;
+    }
 }
 
-uint32_t
-rb_filled_space(ring_t * rb)
-{
-    if (rb == NULL)
-        return 0;
-    if (rb->round_complete == 1)
-        return( (rb->buffer_end -  rb->tail)  +  ( rb->head - rb->buffer_start) );
-    
-    else
-        return(rb->head - rb->tail);
-}
 
 void
 rb_release(ring_t * rb)
@@ -171,6 +194,7 @@ rb_release(ring_t * rb)
         return;
 
     if (rb->buffer_start != NULL)
-    	free(rb->buffer_start);
+    free(rb->buffer_start);
+
     free(rb);
 }
--- ring_buffer.h
+++ ring_buffer.h
@@ -1,19 +1,16 @@
-//#include <mutex>
-//#include <pthreads.h>
 #include <stdint.h>
 
-typedef struct ring_t//some padding should be taking into consideration.
+typedef struct ring_t           //some padding should be taking into consideration.
 {
-    uint8_t *buffer_start;//ptr to the start address
-    uint8_t *buffer_end;//ptr to the end mem address allocated 
-    uint8_t *head;//ptr to the head memory address
-    uint8_t *tail;//ptr to the tail memory address
-    uint32_t size;//Total buffer size
-    uint32_t read_calls;//for debugging
-    uint32_t write_calls;//for debugging
-    //pthread_mutex_t mtx;
-    //std::mutex mtx;           // mutex for critical section
-    char round_complete; //0- false; 1- true
+    uint8_t *buffer_start;      //ptr to the start address
+    uint8_t *buffer_end;        //ptr to the end mem address allocated 
+    uint8_t *head;              //ptr to the head memory address
+    uint8_t *tail;              //ptr to the tail memory address
+    uint32_t size;              //Total buffer size
+    uint32_t read_calls;        //for debugging
+    uint32_t write_calls;       //for debugging
+    uint32_t available_size;    //numero de dados por ler
+    char round_complete;        //0- false; 1- true
 }ring_t;
 
 ring_t* rb_new(uint32_t buffer_len, uint32_t pointer_size);
@@ -24,13 +21,13 @@ uint32_t rb_empty(ring_t* rb);
 
 uint32_t rb_write(ring_t* rb, uint8_t *data, uint32_t size);
 
-uint32_t 	rb_read(ring_t* rb, uint32_t *size, uint8_t* memory);
+uint32_t rb_read(ring_t* rb, uint32_t *size, uint8_t* memory);
 
-uint32_t 	rb_free_space(ring_t * rb);
+uint32_t rb_free_space(ring_t * rb);
 
-uint32_t 	rb_filled_space(ring_t * rb);
+//uint32_t rb_filled_space(ring_t * rb);
 
-void	rb_release(ring_t * rb);
+void rb_release(ring_t * rb);
 
 
 
